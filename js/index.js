fc247757a993da62f50e471f8a02a380c7401207
MrMorales <mrphysics3.14@gmail.com>
2023-10-04 22:46:04 -0400

    Server & Style change

--- js/index.js
+++ js/index.js
@@ -8,4 +8,18 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get( "/" , (req, res) => {
+    res.render("public/index.html")
+});
+
+
+
+app.listen(port, () => {
+    console.log("server running successfully on port 3000 dragon");
+});
