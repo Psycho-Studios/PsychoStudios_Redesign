a3ec5424f8a662cde9e9273c1fdb21a81dc68f37
Jeshawn <jeshawnchrite@gmail.com>
2023-11-11 16:51:27 -0500

    Added HTTPS functionality

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-import express from "express";
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+
+// Create a service (the app object is just a callback).
+var app = express();
+
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(443);
+
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
