f88ca1500699059130e7193212e515388461331b
Jeshawn <jeshawnchrite@gmail.com>
2023-11-11 16:34:53 -0500

    Restored import statements.

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var dirname = "/";
-var fileURLToPath = require("url");
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const log = console.log;
-const path = dirname(fileURLToPath(require("meta.url")));
+const path = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
-
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -18,12 +17,10 @@ app.get( "/" , (req, res) => {
     res.sendFile( path , + "index.html")
 });
 
-
 app.post("/email", (req, res) => {
     log('data:', req.body);
     res.sendFile(path , "/public/index.html");
   });
-
 
 app.listen(port, () => {
     log("server running successfully on port 3000 dragon");
