d0f0bd4ce7bea0d7244defd0748442042d1f1e33
MrMorales <mrphysics3.14@gmail.com>
2024-02-13 00:48:58 -0500

    "secruity patch"

--- routes/routers.js
+++ routes/routers.js
@@ -1,9 +1,11 @@
-import express from 'express'; 
 import bodyParser from 'body-parser';
 import { check, validationResult } from 'express-validator';
-import  path  from 'path';
+import dotenv from 'dotenv'; 
 import { dirname } from 'path'; 
-import { fileURLToPath } from 'url';
+import express from 'express'; 
+
+
+import { fileURLToPath } from 'url';import  path  from 'path';
 export const router = express.Router(); // router functions created and exported
 
 
@@ -17,7 +19,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-
+dotenv.config(); // to use the .env file  
 
 router.get( "/" , (req, res,) => {
     res.sendFile( path.join(__dirname , "../views/index.html"));
@@ -33,25 +35,25 @@ router.get( "/success" , (req, res) => {
 
 router.post( "/email", 
 
-        [
-          check('name', 'Name is required').not().isEmpty(), 
-          check('email', 'Email is required').isEmail(),
-          check('phone', 'Phone is required').not().isEmpty(),
-          check('message', 'Message is required').not().isEmpty()
-        ] , 
-        async (req, res) => {
-          const errors = validationResult(req); 
+      [
+        check('name', 'Name is required').not().isEmpty(), 
+        check('email', 'Email is required').isEmail(),
+        check('phone', 'Phone is required').not().isEmpty(),
+        check('message', 'Message is required').not().isEmpty()
+      ] , 
+      async (req, res) => {
+        const errors = validationResult(req); 
           if (!errors.isEmpty()) {
             console.log(errors, "One or more fields are not correctly filled");
             return res.redirect('/failure');
           }
 
-      try {
-        const { name , email , phone , message } = req.body;
-        console.log('user data:', req.body);
-        res.redirect('/success');
-      } catch (error) {
-        console.error(error, "One or more fields are not correctly filled");
-        res.redirect('/failure');
-      }
-    });
+    try {
+      const { name , email , phone , message } = req.body;
+      console.log('user data:', req.body);
+      res.redirect('/success');
+    } catch (error) {
+      console.error(error);
+      res.redirect('/failure');
+    }
+});
