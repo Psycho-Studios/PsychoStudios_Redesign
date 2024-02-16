11959ac7aabe1ae8581e184e00a37f4460d09b52
MrMorales <mrphysics3.14@gmail.com>
2024-02-16 01:42:45 -0500

    "server mail hanler logic near completion"

--- routes/routers.js
+++ routes/routers.js
@@ -3,7 +3,7 @@ import { check, validationResult } from 'express-validator';
 import dotenv from 'dotenv'; 
 import { dirname } from 'path'; 
 import express from 'express'; 
-
+import nodemailer from 'nodemailer';
 
 import { fileURLToPath } from 'url';import  path  from 'path';
 export const router = express.Router(); // router functions created and exported
@@ -20,6 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 dotenv.config(); // to use the .env file  
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // to avoid self-signed certificate error 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: { 
+        user: process.env.EMAIL, 
+        pass: process.env.PASSWORD
+    }
+  });
+
 
 router.get( "/" , (req, res,) => {
     res.sendFile( path.join(__dirname , "../views/index.html"));
@@ -44,16 +53,34 @@ router.post( "/email",
       async (req, res) => {
         const errors = validationResult(req); 
           if (!errors.isEmpty()) {
-            console.log(errors, "One or more fields are not correctly filled");
+            log(errors, "One or more fields are not correctly filled");
             return res.redirect('/failure');
           }
 
     try {
+      // Takes user data and creates a mailOptions object for nodemailer
       const { name , email , phone , message } = req.body;
-      console.log('user data:', req.body);
-      res.redirect('/success');
+        const mailOptions = {
+          from: req.body.email,
+          to: process.env.EMAIL,
+          subject: 'New Message from PsychoStudios.com',
+          contactInfo: req.body.phone + ' ' + req.body.name,
+          text: req.body.message
+        };
+
+        // logic that handles transportation and errors 
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            log(error);
+            res.redirect('/failure');
+          } else {
+            console.log('Email sent: ' + info.response);
+            res.redirect('/success');
+          }
+        });
+          log('user data:', req.body);
     } catch (error) {
-      console.error(error);
+      log.error(error);
       res.redirect('/failure');
     }
 });
