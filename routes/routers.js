13424fd04bdeedbf316e0739c743fe9e9ddbabf4
MrMorales <mrphysics3.14@gmail.com>
2024-09-22 16:04:25 -0400

    "Working Tree fix."

--- routes/routers.js
+++ routes/routers.js
@@ -76,7 +76,6 @@ router.post(
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           log(error);
-          log("From line 76");
           res.redirect("/failure");
         } else {
           console.log("Email sent: " + info.response);
